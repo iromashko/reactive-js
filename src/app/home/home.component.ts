8710432eabd3d9e8bcfd4a1054fb10584b59119d
iromashko <iromashko@me.com>
2020-11-17 14:30:34 +0300

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from "@angular/core";
+import { Course, sortCoursesBySeqNo } from "../model/course";
+import { interval, noop, Observable, of, throwError, timer } from "rxjs";
+import {
+  catchError,
+  delay,
+  delayWhen,
+  filter,
+  finalize,
+  map,
+  retryWhen,
+  shareReplay,
+  tap,
+} from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
+import { CoursesService } from "../services/courses.service";
+import { LoadingService } from "../services/loading.service";
+
+@Component({
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit {
+  beginnerCourses$: Observable<Course[]>;
+
+  advancedCourses$: Observable<Course[]>;
+
+  constructor(
+    private coursesService: CoursesService,
+    private loadingService: LoadingService
+  ) {}
+
+  ngOnInit() {
+    this.reloadCourses();
+  }
+
+  reloadCourses() {
+    this.loadingService.loadingOn();
+
+    const courses$ = this.coursesService
+      .loadAllCourses()
+      .pipe(map((courses) => courses.sort(sortCoursesBySeqNo)));
+
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted<Course[]>(
+      courses$
+    );
+
+    this.beginnerCourses$ = loadCourses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === "BEGINNER")
+      )
+    );
+
+    this.advancedCourses$ = loadCourses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category === "ADVANCED")
+      )
+    );
+  }
+}
